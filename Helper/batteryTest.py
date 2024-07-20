537dc0373440c29e256dcf69eab25c95236c5323
JohnGottiGE <joerg.gotthardt@gmx.de>
2024-07-20 11:25:58 +0200

    add battery test

---
+++ Helper/batteryTest.py
@@ -0,0 +1,10 @@
+from ..Program.my_robot import *
+
+# Just testing the battery values
+# and print them out
+
+br = my_robot()
+
+print("current: " + str(br.hub.battery.current()))
+print("voltage: " + str(br.hub.battery.voltage()))
+print("Normal fully charged voltage is around 8000")
